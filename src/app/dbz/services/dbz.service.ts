d0930d7e5b5624a06c1027a7435f3fce9d6d92a7
jair <jairdeyner730791@gmail.com>
2022-10-23 19:22:36 -0500

    Final

---
+++ src/app/dbz/services/dbz.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+
+import { Personaje } from '../interfaces/dbz.interface';
+
+@Injectable()
+export class DbzService {
+  private _personajes: Personaje[] = [
+    {
+      nombre: 'Goku',
+      poder: 15000,
+    },
+    {
+      nombre: 'Vegeta',
+      poder: 7500,
+    },
+  ];
+
+  constructor() {
+    console.log('servicio inicializado');
+  }
+
+  get personajes(): Array<Personaje> {
+    return [...this._personajes];
+  }
+
+  agregarPersonaje(personaje: Personaje): void {
+    this._personajes.push(personaje);
+  }
+}
